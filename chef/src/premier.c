fc6b9e3317d9349a03b545dc764abb0f283f1ccc
Geoffrey Frogeye <geoffrey@frogeye.fr>
2018-02-12 19:23:24 +0100

    Communication A↔C : Base côté Chef

--- chef/src/premier.c
+++ chef/src/premier.c
@@ -1,7 +1,15 @@
 #include "premier.h"
 
-int main() {
-    printf("Hello world!\n");
-    // avancer(10);
+#include "serial.h"
+
+int main()
+{
+    unsigned char g;
+    configureCA();
+    sendByteCA('c');
+    while ((g = readByteCA())) {
+        printf("%c\n", g);
+    }
+    deconfigureCA();
     return 0;
 }
